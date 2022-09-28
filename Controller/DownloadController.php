52e42e85fdf30cd142b041cd403e3e7970082986
Peter Nassef <peter.nassef@gmail.com>
2022-09-28 11:28:12 +0200

    Fix DownloadController

--- Controller/DownloadController.php
+++ Controller/DownloadController.php
@@ -3,9 +3,10 @@
 namespace PN\MediaBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
-use PN\MediaBundle\Entity\Document;
+use PN\ServiceBundle\Service\ContainerParameterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -15,24 +16,39 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DownloadController extends AbstractController
 {
+    private EntityManagerInterface $em;
+    private int $documentId; // document id from document Entity
+    private string $documentClass;
 
-    private $documentId; // document id from document Entity
-
-    public function __construct()
+    public function __construct(ContainerParameterService $containerParameterService, EntityManagerInterface $em)
     {
-        $request = Request::createFromGlobals();
-        $getParameter = str_replace("'", '"',
-            $request->query->get('d')); // ex: {{ path('download', {'d': '{"document":'document.id'}'}) }}
-        $parameter = json_decode($getParameter, true);
-        $this->documentId = $parameter['document'];
+        $this->em = $em;
+        $this->documentClass = $containerParameterService->get('pn_media_document.document_class');
     }
 
-    public function getDownloadNameAndPath(EntityManagerInterface $em)
+
+    /**
+     * @Route("/", name="download", methods={"GET"})
+     */
+    public function download(Request $request): Response
     {
+        $nameAndPath = $this->getDownloadNameAndPath($request);
+        $path = $nameAndPath->path;
+        $name = str_replace("/", "-", $nameAndPath->name);
+        if (!file_exists($path)) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->file($path, $name);
+    }
+
+    private function getDownloadNameAndPath(Request $request): \stdClass
+    {
+        $this->extractDataFromRequest($request);
         if ($this->documentId == null) {
             throw $this->createNotFoundException();
         }
-        $entity = $em->getRepository(Document::class)->find($this->documentId);
+        $entity = $this->em->getRepository($this->documentClass)->find($this->documentId);
         if (!$entity) {
             throw $this->createNotFoundException();
         }
@@ -45,30 +61,19 @@ class DownloadController extends AbstractController
                 $fileName = $originalEntity->getName().".".$entity->getNameExtension();
             }
         }
+
         $return = new \stdClass();
-
-
         $return->name = $fileName;
         $return->path = $entity->getAssetPath();
 
         return $return;
     }
 
-    /**
-     * test page.
-     *
-     * @Route("/", name="download", methods={"GET"})
-     */
-    public function downloadAction(EntityManagerInterface $em)
+    private function extractDataFromRequest(Request $request): void
     {
-        $nameAndPath = $this->getDownloadNameAndPath($em);
-        $path = $nameAndPath->path;
-        $name = str_replace("/", "-", $nameAndPath->name);
-        if (!file_exists($path)) {
-            throw $this->createNotFoundException();
-        }
-
-        return $this->file($path, $name);
+        $getParameter = str_replace("'", '"',
+            $request->query->get('d')); // ex: {{ path('download', {'d': '{"document":'document.id'}'}) }}
+        $parameter = json_decode($getParameter, true);
+        $this->documentId = $parameter['document'];
     }
-
 }
